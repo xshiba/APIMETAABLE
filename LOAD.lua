14b8ee43fa3e6934762d55cb48e65c9ff43676c1
xshiba <92438125+xshiba@users.noreply.github.com>
2024-02-28 19:19:17 +0700

    Update LOAD.lua

--- LOAD.lua
+++ LOAD.lua
@@ -1,87 +1,89 @@
 local mt = getrawmetatable(game)
 setreadonly(mt,false)
 local old = mt.__namecall
-mt.__namecall = newcclosure(function(...)
-    local method = getnamecallmethod()
-    local args = {...}
-    if not MagnetDoughtDF and
-    not EnabledAttackSeaB and
-    not SeaEventsEnabled then
-        return old(unpack(args))
-    end
-    if tostring(method) == "FireServer" then
-        if tostring(args[1]) == "RemoteEvent" then
-            if tostring(args[2]) ~= "true" and tostring(args[2]) ~= "false" then
-                if _G.AutoFarm and MagnetDoughtDF then
-                    if PosMonDoughtOpenDoorDF ~= nil then
-                        if tostring(typeof(args[2])) == "CFrame" then
-                            args[2] = PosMonDoughtOpenDoorDF
-                        elseif tostring(typeof(args[2])) == "Vector3" then
-                            args[2] = PosMonDoughtOpenDoorDF.Position
-                        end
-                        return old(unpack(args))
-                    end
-                end
-                if _G.AutoFarm and EnabledAttackSeaB then
-                    if PosSeaB ~= nil then
-                        if tostring(typeof(args[2])) == "CFrame" then
-                            args[2] = PosSeaB
-                        elseif tostring(typeof(args[2])) == "Vector3" then
-                            args[2] = PosSeaB.Position
-                        end
-                        return old(unpack(args))
-                    end
-                end
-                if _G.AutoFarm and SeaEventsEnabled then
-                    if SeaEventsPos ~= nil then
-                        if tostring(typeof(args[2])) == "CFrame" then
-                            args[2] = SeaEventsPos
-                        elseif tostring(typeof(args[2])) == "Vector3" then
-                            args[2] = SeaEventsPos.Position
-                        end
-                        return old(unpack(args))
-                    end
-                end
-               
-            end
+task.spawn(function()
+    mt.__namecall = newcclosure(function(...)
+        local method = getnamecallmethod()
+        local args = {...}
+        if not MagnetDoughtDF and
+        not EnabledAttackSeaB and
+        not SeaEventsEnabled then
+            return old(unpack(args))
         end
-    elseif tostring(method) == "InvokeServer" then
-        if tostring(args[1]) == "" then
-            if tostring(args[2]) ~= "true" and tostring(args[2]) ~= "false" then
-                if _G.AutoFarm and MagnetDoughtDF then
-                    if PosMonDoughtOpenDoorDF ~= nil then
-                        if tostring(typeof(args[3])) == "CFrame" then
-                            args[3] = PosMonDoughtOpenDoorDF
-                        elseif tostring(typeof(args[3])) == "Vector3" then
-                            args[3] = PosMonDoughtOpenDoorDF.Position
+        if tostring(method) == "FireServer" then
+            if tostring(args[1]) == "RemoteEvent" then
+                if tostring(args[2]) ~= "true" and tostring(args[2]) ~= "false" then
+                    if _G.AutoFarm and MagnetDoughtDF then
+                        if PosMonDoughtOpenDoorDF ~= nil then
+                            if tostring(typeof(args[2])) == "CFrame" then
+                                args[2] = PosMonDoughtOpenDoorDF
+                            elseif tostring(typeof(args[2])) == "Vector3" then
+                                args[2] = PosMonDoughtOpenDoorDF.Position
+                            end
+                            return old(unpack(args))
                         end
-                        return old(unpack(args))
                     end
+                    if _G.AutoFarm and EnabledAttackSeaB then
+                        if PosSeaB ~= nil then
+                            if tostring(typeof(args[2])) == "CFrame" then
+                                args[2] = PosSeaB
+                            elseif tostring(typeof(args[2])) == "Vector3" then
+                                args[2] = PosSeaB.Position
+                            end
+                            return old(unpack(args))
+                        end
+                    end
+                    if _G.AutoFarm and SeaEventsEnabled then
+                        if SeaEventsPos ~= nil then
+                            if tostring(typeof(args[2])) == "CFrame" then
+                                args[2] = SeaEventsPos
+                            elseif tostring(typeof(args[2])) == "Vector3" then
+                                args[2] = SeaEventsPos.Position
+                            end
+                            return old(unpack(args))
+                        end
+                    end
+                   
                 end
-                if _G.AutoFarm and EnabledAttackSeaB then
-                    if PosSeaB ~= nil then
-                        if tostring(typeof(args[3])) == "CFrame" then
-                            args[3] = PosSeaB
-                        elseif tostring(typeof(args[3])) == "Vector3" then
-                            args[3] = PosSeaB.Position
-                        end
-                        return old(unpack(args))
-                    end
-                end
-                if _G.AutoFarm and SeaEventsEnabled then
-                    if SeaEventsPos ~= nil then
-                        if tostring(typeof(args[3])) == "CFrame" then
-                            args[3] = SeaEventsPos
-                        elseif tostring(typeof(args[3])) == "Vector3" then
-                            args[3] = SeaEventsPos.Position
-                        end
-                        return old(unpack(args))
-                    end
-                end              
             end
-        end
-    end 
-    return old(...)
+        elseif tostring(method) == "InvokeServer" then
+            if tostring(args[1]) == "" then
+                if tostring(args[2]) ~= "true" and tostring(args[2]) ~= "false" then
+                    if _G.AutoFarm and MagnetDoughtDF then
+                        if PosMonDoughtOpenDoorDF ~= nil then
+                            if tostring(typeof(args[3])) == "CFrame" then
+                                args[3] = PosMonDoughtOpenDoorDF
+                            elseif tostring(typeof(args[3])) == "Vector3" then
+                                args[3] = PosMonDoughtOpenDoorDF.Position
+                            end
+                            return old(unpack(args))
+                        end
+                    end
+                    if _G.AutoFarm and EnabledAttackSeaB then
+                        if PosSeaB ~= nil then
+                            if tostring(typeof(args[3])) == "CFrame" then
+                                args[3] = PosSeaB
+                            elseif tostring(typeof(args[3])) == "Vector3" then
+                                args[3] = PosSeaB.Position
+                            end
+                            return old(unpack(args))
+                        end
+                    end
+                    if _G.AutoFarm and SeaEventsEnabled then
+                        if SeaEventsPos ~= nil then
+                            if tostring(typeof(args[3])) == "CFrame" then
+                                args[3] = SeaEventsPos
+                            elseif tostring(typeof(args[3])) == "Vector3" then
+                                args[3] = SeaEventsPos.Position
+                            end
+                            return old(unpack(args))
+                        end
+                    end              
+                end
+            end
+        end 
+        return old(...)
+    end)
 end)
 task.spawn(function()
     while true do wait()
